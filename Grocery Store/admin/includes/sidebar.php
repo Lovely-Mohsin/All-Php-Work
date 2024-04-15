db7cf0e44a9b16ebada579930c8e783d2b7c506a
Lovely-Mohsin <153726650+Lovely-Mohsin@users.noreply.github.com>
2024-04-14 22:32:25 -0700

    Grocery Store Created
    Grocery store Created

---
+++ Grocery Store/admin/includes/sidebar.php
@@ -0,0 +1,33 @@
+        <!--**********************************
+            Sidebar start
+        ***********************************-->
+        <div class="nk-sidebar">           
+            <div class="nk-nav-scroll">
+
+                <ul class="metismenu" id="menu">
+                    <li class="nav-label">Dashboard</li>
+                    <li>
+                        <a  href="javascript:void()" aria-expanded="false">
+                            <i class="icon-speedometer menu-icon"></i><span class="nav-text">Dashboard</span>
+                        </a>
+
+                    </li>
+
+             
+                        <li>
+                        <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                            <i class="icon-menu menu-icon"></i><span class="nav-text">Table</span>
+                        </a>
+                        <ul aria-expanded="false">
+                            <li><a href="./table-basic.html" aria-expanded="false">Basic Table</a></li>
+                            <li><a href="./table-datatable.html" aria-expanded="false">Data Table</a></li>
+                        </ul>
+                    </li>
+
+
+                </ul>
+            </div>
+        </div>
+        <!--**********************************
+            Sidebar end
+        ***********************************-->
---
+++ Grocery Store/admin/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Dashboard - Home</title>
+
+    <!-- css-links include -->
+    <?php require_once("./includes/css-links.php") ?>
+
+</head>
+
+<body>
+
+    <!-- navbar include -->
+    <?php require_once("./includes/navbar.php") ?>
+
+    <!-- sidebar include -->
+    <?php require_once("./includes/sidebar.php") ?>
+
+    <div class="content-body">
+
+        <div class="container-fluid mt-3">
+
+            <!-- statics tabs include -->
+            <?php require_once("./includes/statics-tabs.php") ?>
+
+        </div>
+
+        </div> <!--*** Main wrapper end *****-->
+
+    <!-- footer include -->
+    <?php require_once("./includes/footer.php")  ?>
+
+    <!-- javascript links include -->
+    <?php require_once("./includes/javascript-links.php")  ?>
+</body>
+
+</html>
